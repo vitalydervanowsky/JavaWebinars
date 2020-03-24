141a8cb07b5ed640c13cce2e88f0795e6d27d0d5
Vitali Dzervanouski <vitalydervanowsky@gmail.com>
2020-03-24 12:06:47 +0300

    feat: add Task3 in Webinar3

--- src/by/dzrvnsk/Webinar3.java
+++ src/by/dzrvnsk/Webinar3.java
@@ -1,7 +1,49 @@
 package by.dzrvnsk;
 
+import java.util.Scanner;
+
 public class Webinar3 {
+    // Task 3
     public static void main(String[] args) {
-        
+        int length = 5;
+        int[] a;
+        Webinar3 w = new Webinar3();
+        a = w.input(length);
+        w.print(a);
+        w.sort(a);
+        w.print(a);
+    }
+    public int[] input(int l) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите " + l + " чисел:");
+        int[] arr = new int[l];
+        for (int i = 0; i < l; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("initializing of array...");
+        return arr;
+    }
+    public void print(int[] arr) {
+        System.out.println("result:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i == arr.length - 1) {
+                System.out.println();
+            } else {
+                System.out.print(" | ");
+            }
+        }
+    }
+    public void sort(int[] arr) {
+        System.out.println("sorting...");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
